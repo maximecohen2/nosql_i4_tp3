cd124a178e82b5596f92824f96bcdaa7b2f6cf12
YannG <Yann4.g@gmail.com>
2019-04-18 08:08:54 +0200

    [rebuild functions] everything functional up to delete

--- php/function.php
+++ php/function.php
@@ -1,16 +1,38 @@
 <?php
 /* Functions for redis operations */
 
-function openRedisConnection($redis, $hostName, $port){
+function openRedisConnection(){
     // Opening a redis connection
-    $redis->connect( $hostName, $port );
+    $redis = new redis();
+    $host = "redis";
+    $port = 6379;
+    $redis->connect($host, $port);
+    return $redis;
 }
 
-function postNote( $redis, $key, $note ) {
+function closeRedisConnection($redis) {
+    //close redis connection
+    $redis->close();
+}
+
+function postNote($redis, $key, $note) {
     try {
-        $redis->set( $key, $note);
-        echo "you have deleted the note: ".$key;
+        // associate a new note with a key
+        $redis->set($key, $note);
+        return $key;
     } catch( Exception $e ){
+        echo $e->getMessage();
+    }
+}
+
+function getLastKey($redis) {
+    try {
+        // get the last key that was posted
+        $keyListArr = $redis->keys('*');//getNoteList($redis);
+        asort($keyListArr);
+        $lastKey = end($keyListArr);
+        return $lastKey;
+    }catch(Exception $e){
         echo $e->getMessage();
     }
 }
@@ -19,10 +41,14 @@ function getNoteList($redis){
     try {
         // getting a list of all the keys from redis
         $keyList = $redis->keys('*');
+        $noteList = array();
         foreach ($keyList as $key) {
-            echo $key."\n";
+            $note = $redis->get($key);
+            $noteList[$key] = $note;
         }
-    }catch( Exception $e ){
+        ksort($noteList);
+        return $noteList;
+    }catch(Exception $e){
         echo $e->getMessage();
     }
 }
@@ -30,9 +56,9 @@ function getNoteList($redis){
 function getNote($redis, $key){
     try{
         // get note from its key
-        $note = $redis->get( $key );
-        echo $note;
-    }catch( Exception $e ){
+        $note = $redis->get($key);
+        return $note;
+    }catch(Exception $e){
         echo $e->getMessage();
     }
 }
@@ -41,23 +67,25 @@ function getNote($redis, $key){
 function deleteNote($redis, $key){
     try{
         // deleting note from its key
-        $redis->del( $key);
-        echo "you have deleted the note: ".$key;
-    }catch( Exception $e ){
+        $redis->del($key);
+    }catch(Exception $e){
         echo $e->getMessage();
     }
 }
 
-function postNoteJSON($redis, $key, $note) {
+function postNoteJSON($redis, $author, $key, $note) {
     try {
         //$redis = new redis();
 
+        $currDate = date('l jS \of F Y h:i:s A');
+        $noteArray = array('author'=>$author, 'created'=> $currDate, 'note'=>$note);
+
+        $objJSON = json_encode($noteArray);
         $redis->rawCommand("JSON");
-    }catch( Exception $e ){
+    }catch(Exception $e){
         echo $e->getMessage();
     }
 }
-
 
 
 ?>
--- php/index.php
+++ php/index.php
@@ -7,21 +7,65 @@ $config = ['settings' => [
 ]];
 $app = new \Slim\App($config);
 
+// include redis function file
+include 'function.php';
+
 // Define app routes
 $app->get('/notes', function ($request, $response, $args) {
-    return $response->write("Hello Notes");
+    // init redis connection
+    $redis = openRedisConnection();
+
+    // create the note list as an array
+    $noteList = getNoteList($redis);
+
+    // close connection
+    closeRedisConnection($redis);
+
+    return $response->write(print_r($noteList, true));
 });
 
 $app->post('/notes', function ($request, $response, $args) {
-    return $response->write($key);
+    // init redis connection
+    $redis = openRedisConnection();
+
+    // post a new note to redis in text/plain
+    $key = getLastKey($redis);
+    $key += 1;
+    $note = $request->getBody()->getContents();
+
+    $posted = postNote($redis, $key, $note);
+
+    // close connection
+    closeRedisConnection($redis);
+
+    return $response->write($posted);
 });
 
 $app->get('/notes/{idnote}', function ($request, $response, $args) use ($app) {
-    return $response->write("Hello " . $args['idnote']);
+    // init redis connection
+    $redis = openRedisConnection();
+
+    // get a note based on its id
+    $note = getNote($redis, $args['idnote']);
+
+    // close connection
+    closeRedisConnection($redis);
+
+    return $response->write(print_r($note, true));
 });
 
 $app->delete('/notes/{idnote}', function ($request, $response, $args) use ($app) {
-    $app->response->redirect('/notes');
+    // init redis connection
+    $redis = openRedisConnection();
+
+    // delete a note from redis db base don its id
+    $key = $args['idnote'];
+    deleteNote($redis, $key);
+
+    // close connection
+    closeRedisConnection($redis);
+
+    return $response->withRedirect('/notes');
 });
 
 // Run app
